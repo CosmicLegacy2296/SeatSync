8a7ba08db95db5fe58062762566c9a76b8e43b08
scoopiezz <zayaan.hamid.zh@gmail.com>
2026-06-05 10:14:06 -0400

    Move existing tables into seatsync schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,8 @@
 CREATE SCHEMA IF NOT EXISTS seatsync;
+
+ALTER TABLE IF EXISTS public.booking_extension_requests SET SCHEMA seatsync;
+ALTER TABLE IF EXISTS public.bookings SET SCHEMA seatsync;
+ALTER TABLE IF EXISTS public.companies SET SCHEMA seatsync;
+ALTER TABLE IF EXISTS public.workspace_desks SET SCHEMA seatsync;
+ALTER TABLE IF EXISTS public.workspace_floors SET SCHEMA seatsync;
+ALTER TABLE IF EXISTS public.users SET SCHEMA seatsync;
